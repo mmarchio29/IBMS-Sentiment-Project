b256ee47c37c91175d920ed22054e7e192fb532d
David Euijoon Kim <dk98@bu.edu>
2024-08-06 00:20:49 -0400

    update Aug 6

--- HerokuDeployment(August)/layouts/report.py
+++ HerokuDeployment(August)/layouts/report.py
@@ -2,6 +2,6 @@ from dash import html, dcc
 
 def layout():
     return html.Div([
-        html.H1('Home Page', className='home-title'),
-        dcc.Link('Go to Analysis', href='/main-analysis', className='link-button')
+        html.H1('Here is the main analysis report', className='home-title'),
+        dcc.Link('Go back', href='/', className='link-button')
     ], className='page-content')
